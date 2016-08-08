2c9f03db21d68b4592134061b5aa47ec1299c700
Dottie Kessler <db4871@us.att.com>
2016-08-08 19:33:59 -0400

    fixed spacing and added new recursion function for factorials

---
+++ FactorialUsingRecursion.java
@@ -0,0 +1,27 @@
+// dak 8/8/16
+import java.util.Scanner;
+
+public class FactorialUsingRecursion {
+	public static void main(String[] args) {
+		// eg 4! = 4*3*2*1 = 24
+		// eg 7! = 7*6*5*4*3*2*1 = 5040
+		// eg 1! = 1*1 = 1
+
+		System.out.println("Enter the number to perform factorial");
+		Scanner input = new Scanner(System.in);
+		int numberForFactorial = input.nextInt();
+		System.out.println(factorial(numberForFactorial));
+	}
+
+	private static int factorial(int value) {
+		// System.out.printf("value is %d\n", value);
+		// you need to provide a way for the method to stop calling itself
+		if (value == 1) {
+			return 1;
+		}
+		return factorial(value - 1) * value;
+	}
+}
+
+// Tower of Hanoi is another puzzle where you can use a recursion solution
+
--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,4 @@
+
 // a program to print the fibonacci sequence
 // 7/19/16 dak
 // Help at: http://www.java2novice.com/java-interview-programs/
@@ -6,33 +7,33 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 		System.out.println("Enter the limit for the sequence");
-    	Scanner input = new Scanner(System.in);
-    	int limit = input.nextInt();
-        
-    	// the start of the sequence 0 1 1
-    	int currentMinusOne = 1;
-        int currentMinusTwo = 0;
-         
-         // print out the start and decrease the loop counter by the first 2
-         System.out.print(currentMinusTwo + " " + currentMinusOne + " ");
-         limit -=2;
-         
-         // start at the 3rd value which is a 1: 0 1 1
-         int current = 1;
-         for(int i=0; i < limit; i++) {
-        	 	current = currentMinusOne + currentMinusTwo;
-        	 	System.out.print(current + " " );
-        	 	
-        	 	// set up for next iteration 
-        	 	currentMinusTwo = currentMinusOne;
-        	 	currentMinusOne = current;
-         }
-         
-         // close the input
-         input.close();
-    }
+		Scanner input = new Scanner(System.in);
+		int limit = input.nextInt();
+
+		// the start of the sequence 0 1 1
+		int currentMinusOne = 1;
+		int currentMinusTwo = 0;
+
+		// print out the start and decrease the loop counter by the first 2
+		System.out.print(currentMinusTwo + " " + currentMinusOne + " ");
+		limit -= 2;
+
+		// start at the 3rd value which is a 1: 0 1 1
+		int current = 1;
+		for (int i = 0; i < limit; i++) {
+			current = currentMinusOne + currentMinusTwo;
+			System.out.print(current + " ");
+
+			// set up for next iteration
+			currentMinusTwo = currentMinusOne;
+			currentMinusOne = current;
+		}
+
+		// close the input
+		input.close();
+	}
 }
